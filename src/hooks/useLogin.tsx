8ee5493e3a766f8f968dad7de92f96bfdd564641
r-i-c-h <rich@richwerden.com>
2022-03-28 00:17:53 -0400

    🆙useLogin + implements Login page/form

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { projectAuth } from '../firebase/config'
+import { projectAuth, projectFirestore } from '../firebase/config'
 import { useAuthContext } from './useAuthContext';
 import { handleError } from '../ts/ErrorHandler';
 
@@ -16,7 +16,9 @@ export const useLogin = () => {
     try {
       const res = await projectAuth.signInWithEmailAndPassword(email, password);
 
-      if (!res) { throw new Error('Could not Login') }
+      if (!res || !res.user) { throw new Error('Could not Login') }
+
+      await projectFirestore.collection('users').doc(res.user.uid).update({ online: true })
 
       dispatch!({ type: 'LOGIN', payload: res.user })
 
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,43 @@
+import { FormEvent, useState } from 'react';
+import { useLogin } from '../../hooks/useLogin';
+import { handleError } from '../../ts/ErrorHandler'
+
 import './Login.scss'
 
 export default function Login() {
+  const { error, isPending, login } = useLogin();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    login(email, password)
+  }
+
   return (
-    <div>
-      Login
-    </div>
+    <form onSubmit={handleSubmit} className="login-form">
+      <h2 className="widget-title">Log In</h2>
+      <label> {/* Email */}
+        <span>Email:</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label> {/* Pwd */}
+        <span>Password:</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      {!isPending && <button className="btn">Login</button>}
+      {isPending && <button className="btn" disabled>Loading</button>}
+      {error && <div className="error">{handleError(error)}</div>}
+    </form>
   )
 }
