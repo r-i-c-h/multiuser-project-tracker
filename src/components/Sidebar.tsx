a3064a12525d3ed04bde4d8dd90da9f5dd2ebeab
r-i-c-h <rich@richwerden.com>
2022-04-07 15:39:58 -0400

    triv: add dynamic user greeting to <Sidebar>

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,6 +8,16 @@ import Icon_Add from '../assets/icon_add.svg';
 
 import './Sidebar.scss'
 
+const getTimeGreeting = () => {
+  const rightNow = new Date();
+  const hour = rightNow.getHours();
+  if (hour === 0 || hour === 1) { return 'Buenas Noches' }
+  if (hour > 1 && hour <= 11) { return 'Good Morning' }
+  if (hour >= 12 && hour <= 16) { return 'Good Afternoon' }
+  if (hour >= 17 && hour <= 23) { return 'Good Evening' }
+  return 'Ahoy hoy'
+}
+
 export default function Sidebar() {
   const { user } = useAuthContext();
   const photoURL = user?.photoURL
@@ -16,7 +26,7 @@ export default function Sidebar() {
       <div className="sidebar-content">
         <div className="user">
           <Avatar src={photoURL} />
-          <p>Greetings {user?.displayName}</p>
+          <p>{getTimeGreeting()}, <br />{user?.displayName}</p>
         </div>
         <nav className="links">
           <ul>
