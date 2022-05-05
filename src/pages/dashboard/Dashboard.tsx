5ecd273998e524f9d1f56a114e1d35c204ed95e3
r-i-c-h <rich@richwerden.com>
2022-05-04 22:45:06 -0400

    add:Starts <Dashboard>

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,22 @@
+import { IProject } from '../../ts/interfaces-and-types';
+import { useCollection } from '../../hooks/useCollection'
 import './Dashboard.scss'
 
 export default function Dashboard() {
+  const { documents, error } = useCollection<IProject>('projects');
+  console.log(documents)
+  console.log(error)
   return (
     <div>
-      Dashboard
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      <ul>
+        {documents &&
+          documents.map((proj: IProject) => {
+            return <li key={proj.id}>{proj.projectName}</li>
+          })
+        }
+      </ul>
     </div>
   )
 }
