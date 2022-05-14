ca6686d6d3f9521f3816ca7ec9872a8c9c137ff1
r-i-c-h <rich@richwerden.com>
2022-05-14 00:27:41 -0400

    add: Loading Animation widget

---
+++ src/components/LoadingAnimation.tsx
@@ -0,0 +1,11 @@
+import './LoadingAnimation.scss'
+
+const LoadingAnimation = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+    </div>
+  )
+}
+
+export default LoadingAnimation;
