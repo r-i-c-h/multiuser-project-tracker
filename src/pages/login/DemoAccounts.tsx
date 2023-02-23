659ec90b572e053854fee9cdd7391821f040e7ed
r-i-c-h <rich@richwerden.com>
2023-02-23 12:08:27 -0500

    triv: Removes one of the demo logins from the display

--- src/pages/login/DemoAccounts.tsx
+++ src/pages/login/DemoAccounts.tsx
@@ -9,7 +9,7 @@ const DemoAccounts = () => (
       <tbody>
         <tr className='usr-acct'><td>📧: <span className="deets">demo@example.com</span></td><td className='usr-acct-pwd'>🔑🔒: <span className="deets">123456</span></td></tr>
         <tr className='usr-acct'><td>📧: <span className="deets">foo@example.com</span></td><td className='usr-acct-pwd'>🔑🔒: <span className="deets">123456</span></td></tr>
-        <tr className='usr-acct'><td>📧: <span className="deets">bar@example.com</span></td><td className='usr-acct-pwd'>🔑🔒: <span className="deets">123456</span></td></tr>
+        {/* <tr className='usr-acct'><td>📧: <span className="deets">bar@example.com</span></td><td className='usr-acct-pwd'>🔑🔒: <span className="deets">123456</span></td></tr> */}
       </tbody>
     </table>
     <p className='usr-acct-warn'>(Though you <em>can</em> <Link to="/signup" className="">signup as a new user</Link>...)</p>
