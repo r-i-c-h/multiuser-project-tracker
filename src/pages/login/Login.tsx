5e5e96729eb45c2cfe1d154d4a07bec541a58245
r-i-c-h <rich@richwerden.com>
2023-02-26 01:22:37 -0500

    adds AnimatedInput to Login Page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -4,12 +4,14 @@ import { useLogin } from '../../hooks/useLogin';
 import { handleError } from '../../ts/ErrorHandler'
 
 import DemoAccounts from './DemoAccounts';
+import AnimatedInput from '../../components/AnimatedInput';
 import './Login.scss'
 
 export default function Login() {
   const { error, isPending, login } = useLogin();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault()
@@ -21,26 +23,22 @@ export default function Login() {
     <form onSubmit={handleSubmit} className="entry-panel">
       <h2>Account Login</h2>
       <DemoAccounts />
-      <label> {/* Email */}
-        <span>Email 📬:</span>
-        <input
-          required
-          type="email"
-          placeholder='Enter Email Address'
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-      </label>
-      <label> {/* Pwd */}
-        <span>Password 🔑🔒:</span>
-        <input
-          required
-          type="password"
-          placeholder='Enter Password'
+      <AnimatedInput
+        id="login-email"
+        type="email"
+        injectedValue={email}
+        labelText="Email Address"
+        placeholder="Please Enter Email Address"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <AnimatedInput
+        id="login-pass"
+        type="password"
+        injectedValue={password}
+        labelText="Password"
+        placeholder="Please Enter Your Password"
           onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-      </label>
+      />
       {!isPending && <button className="btn">Login</button>}
       {isPending && <button className="btn" disabled>Loading</button>}
       {error && <div className="error">{handleError(error)}</div>}
