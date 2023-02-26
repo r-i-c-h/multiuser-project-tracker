498c01d8c5abe2b8451891d02128de414ce3cd5c
r-i-c-h <rich@richwerden.com>
2023-02-26 01:23:26 -0500

    🎨 Demo Accounts/Login widget

--- src/pages/login/DemoAccounts.tsx
+++ src/pages/login/DemoAccounts.tsx
@@ -7,12 +7,12 @@ const DemoAccounts = () => (
     <h3 className='demo-h3'>Demo Accounts:</h3>
     <table>
       <tbody>
-        <tr className='usr-acct'><td>📧: <span className="deets">demo@example.com</span></td><td className='usr-acct-pwd'>🔑🔒: <span className="deets">123456</span></td></tr>
-        <tr className='usr-acct'><td>📧: <span className="deets">foo@example.com</span></td><td className='usr-acct-pwd'>🔑🔒: <span className="deets">123456</span></td></tr>
-        {/* <tr className='usr-acct'><td>📧: <span className="deets">bar@example.com</span></td><td className='usr-acct-pwd'>🔑🔒: <span className="deets">123456</span></td></tr> */}
+        <tr><td>📧: <span className="deets">demo@example.com</span></td><td className='usr-acct-pwd'>🔑🔒: <span className="deets">123456</span></td></tr>
+        <tr><td>📧: <span className="deets">foo@example.com</span></td><td className='usr-acct-pwd'>🔑🔒: <span className="deets">123456</span></td></tr>
+        {/* <tr><td>📧: <span className="deets">bar@example.com</span></td><td className='usr-acct-pwd'>🔑🔒: <span className="deets">123456</span></td></tr> */}
       </tbody>
     </table>
-    <p className='usr-acct-warn'>(Though you <em>can</em> <Link to="/signup" className="">signup as a new user</Link>...)</p>
+    <p className='usr-acct-warn'>(Or you <em>can <Link to="/signup" className="">signup as a new user...</Link></em>)</p>
   </div>
 )
 
